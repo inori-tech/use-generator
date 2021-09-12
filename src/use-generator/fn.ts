3664da293a65c41e9d12fe55817b16f446fb132c
inori-lover <337938188@qq.com>
2021-09-13 00:44:26 +0800

    fix: 修正泛型无法推断得出fn入参类型的问题

--- src/use-generator/fn.ts
+++ src/use-generator/fn.ts
@@ -1,12 +1,14 @@
-export type GeneratorCallback<Param = undefined> = Param extends undefined
+export type GeneratorCallback<Param extends Array<unknown> = unknown[]> = Param extends undefined
   ? (signal: AbortSignal) => Generator | AsyncGenerator
-  : (signal: AbortSignal, param: Param) => Generator | AsyncGenerator;
-export type GeneratorReturnFn<Param = undefined> = Param extends undefined ? () => void : (params: Param) => void;
+  : (signal: AbortSignal, ...param: Param) => Generator | AsyncGenerator;
+export type GeneratorReturnFn<Param extends Array<unknown>> = Param extends undefined
+  ? () => void
+  : (...params: Param) => void;
 
-function useGeneratorFn(cb: GeneratorCallback): GeneratorReturnFn;
-function useGeneratorFn<Param>(cb: GeneratorCallback<Param>): GeneratorReturnFn<Param>;
+function useGeneratorFn(cb: GeneratorCallback): GeneratorReturnFn<[]>;
+function useGeneratorFn<Param extends Array<unknown>>(cb: GeneratorCallback<Param>): GeneratorReturnFn<Param>;
 
-function useGeneratorFn<Param>(cb: GeneratorCallback<Param>): GeneratorReturnFn<Param> {
+function useGeneratorFn<Param extends Array<unknown>>(cb: GeneratorCallback<Param>): GeneratorReturnFn<Param> {
   return function () {} as any;
 }
 
